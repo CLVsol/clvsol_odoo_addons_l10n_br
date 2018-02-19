d909517e7e3a820cd73cbe8e2a8ac6bc872e48d4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-02-19 11:49:15 +0000

    [ADD] clv_abcfarma_medicament

---
+++ clv_abcfarma_medicament/models/abcfarma_medicament.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import fields, models
+
+
+class ABCFarmaMedicament(models.Model):
+    _description = 'ABCFarma Medicament'
+    _name = 'clv.abcfarma.medicament'
+    _inherit = 'clv.medicament.model'
+
+    med_abc = fields.Char(string='MED_ABC')
+    med_ctr = fields.Char(string='MED_CTR')
+    med_lab = fields.Char(string='MED_LAB')
+    lab_nom = fields.Char(string='LAB_NOM')
+    med_des = fields.Char(string='MED_DES')
+    med_apr = fields.Char(string='MED_APR')
+    med_barra = fields.Char(string='MED_BARRA')
+    med_gene = fields.Char(string='MED_GENE')
+    med_negpos = fields.Char(string='MED_NEGPOS')
+    med_princi = fields.Char(string='MED_PRINCI')
+
+    med_uni = fields.Float(string='MED_UNI')
+    med_ipi = fields.Float(string='MED_IPI')
+    med_dtvig = fields.Date(string='MED_DTVIG')
+    exp_13 = fields.Boolean(string='EXP_13')
+    med_regims = fields.Char(string='MED_REGIMS')
+    med_varpre = fields.Char(string='MED_VARPRE')
+
+    med_tipmed = fields.Char(string='MED_TRIPMED')
+    med_ref = fields.Char(string='MED_REF')
+    med_ncm = fields.Char(string='MED_NCM')
+    med_dcb = fields.Char(string='MED_DCB')
+    med_cas = fields.Char(string='MED_CAS')
+    med_atc = fields.Char(string='MED_ATC')
+    med_clater = fields.Char(string='MED_CLATER')
+    med_tarja = fields.Char(string='MED_TARJA')
+    med_por344 = fields.Char(string='MED_POR344')
+    med_tiss = fields.Char(string='MED_TISS')
+    med_conf87 = fields.Char(string='MED_CONF87')
+    med_cap = fields.Char(string='MED_CAP')
+
+    _sql_constraints = [
+        ('med_abc_uniq',
+         'UNIQUE (med_abc)',
+         u'Error! The ABCFarma Code must be unique!'),
+    ]
---
+++ clv_abcfarma_medicament/models/abcfarma_medicament_list_item.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import fields, models
+
+
+class ABCFarmaMedicamentListItem(models.Model):
+    _description = 'ABCFarma Medicament List Item'
+    _name = 'clv.abcfarma.medicament.list.item'
+    _order = 'order'
+
+    list_id = fields.Many2one(
+        comodel_name='clv.abcfarma.medicament.list',
+        string='ABCFarma List',
+        help='ABCFarma List',
+        required=False
+    )
+    medicament_id = fields.Many2one(
+        comodel_name='clv.abcfarma.medicament',
+        string='Medicament',
+        help='ABCFarma Medicament',
+        required=False
+    )
+
+    notes = fields.Text(string='Notes')
+
+    order = fields.Integer(string='Order', default=10)
+
+    med_pco18 = fields.Float(string='MED_PCO18')
+    med_pla18 = fields.Float(string='MED_PLA18')
+    med_fra18 = fields.Float(string='MED_FRA18')
+    med_pco17 = fields.Float(string='MED_PCO17')
+    med_pla17 = fields.Float(string='MED_PLA17')
+    med_fra17 = fields.Float(string='MED_FRA17')
+    med_pco12 = fields.Float(string='MED_PCO12')
+    med_pla12 = fields.Float(string='MED_PLA12')
+    med_fra12 = fields.Float(string='MED_FRA12')
+    med_pco19 = fields.Float(string='MED_PCO19')
+    med_pla19 = fields.Float(string='MED_PLA19')
+    med_fra19 = fields.Float(string='MED_FRA19')
+    med_pcozfm = fields.Float(string='MED_PCOZFM')
+    med_plazfm = fields.Float(string='MED_PLAZFM')
+    med_frazfm = fields.Float(string='MED_FRAZFM')
+    med_pco0 = fields.Float(string='MED_PCO0')
+    med_pla0 = fields.Float(string='MED_PLA0')
+    med_fra0 = fields.Float(string='MED_FRA0')
+
+    med_pla20 = fields.Float(string='MED_PLA20')
+    med_pco20 = fields.Float(string='MED_PCO20')
+    med_fra20 = fields.Float(string='MED_FRA20')
+    med_pla175 = fields.Float(string='MED_PLA175')
+    med_pco175 = fields.Float(string='MED_PCO175')
+    med_fra175 = fields.Float(string='MED_FRA175')
+    med_plaz18 = fields.Float(string='MED_PLAZ18')
+    med_pcoz18 = fields.Float(string='MED_PCOZ18')
+    med_fraz18 = fields.Float(string='MED_FRAZ18')
+    med_plaz17 = fields.Float(string='MED_PLAZ17')
+    med_pcoz17 = fields.Float(string='MED_PCOZ17')
+    med_fraz17 = fields.Float(string='MED_FRAZ17')
+    med_plz175 = fields.Float(string='MED_PLZ175')
+    med_pcz175 = fields.Float(string='MED_PCZ175')
+    med_frz175 = fields.Float(string='MED_FRZ175')
+
+    active = fields.Boolean(string='Active', default=1)
+
+
+class ABCFarmaMedicamentList(models.Model):
+    _inherit = 'clv.abcfarma.medicament.list'
+
+    abcfarma_list_item_ids = fields.One2many(
+        comodel_name='clv.abcfarma.medicament.list.item',
+        inverse_name='list_id',
+        string='ABCFarma List Itens'
+    )
