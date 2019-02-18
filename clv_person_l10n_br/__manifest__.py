e09819a8aed1e7d2ba87d2ba3f92419bfe48e0f5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-18 15:33:34 -0300

    [ADD] clv_person_l10n_br

---
+++ clv_person_l10n_br/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Person - Brazilian Localization',
+    'summary': 'Person Module - Brazilian Localization.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_person',
+        'l10n_br_base',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
---
+++ clv_person_l10n_br/wizard/person_contact_information_updt.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class PersonContactInformationUpdate(models.TransientModel):
+    _inherit = 'clv.person.contact_information_updt'
+
+    @api.multi
+    def do_person_contact_information_updt(self):
+        self.ensure_one()
+
+        super(PersonContactInformationUpdate, self).do_person_contact_information_updt()
+
+        for person in self.person_ids:
+
+            _logger.info(u'%s %s', '>>>>>', person.name)
+
+            person.number = person.ref_address_id.number
+            person.district = person.ref_address_id.district
+            person.l10n_br_city_id = person.ref_address_id.l10n_br_city_id
+
+        return True
