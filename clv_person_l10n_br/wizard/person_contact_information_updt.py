26fe7e1cece42e3efb5f85e6a7c205bc80ce4996
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-12 15:06:53 -0300

    [IMP] clv_person_l10n_br (IMP wizard/person_contact_information_updt.py)

--- clv_person_l10n_br/wizard/person_contact_information_updt.py
+++ clv_person_l10n_br/wizard/person_contact_information_updt.py
@@ -16,14 +16,38 @@ class PersonContactInformationUpdate(models.TransientModel):
     def do_person_contact_information_updt(self):
         self.ensure_one()
 
-        super().do_person_contact_information_updt()
+        # super().do_person_contact_information_updt()
 
+        person_count = 0
         for person in self.person_ids:
 
-            _logger.info(u'%s %s', '>>>>>', person.name)
+            person_count += 1
 
-            person.number = person.ref_address_id.number
-            person.district = person.ref_address_id.district
-            person.l10n_br_city_id = person.ref_address_id.l10n_br_city_id
+            _logger.info(u'%s %s %s', '>>>>>', person_count, person.name)
+
+            if person.ref_address_id is not False:
+
+                values = {}
+
+                values['street'] = person.ref_address_id.street
+                values['street2'] = person.ref_address_id.street2
+                values['country_id'] = person.ref_address_id.country_id.id
+                values['state_id'] = person.ref_address_id.state_id.id
+                values['city'] = person.ref_address_id.city
+                values['zip'] = person.ref_address_id.zip
+                if self.updt_phone:
+                    values['phone'] = person.ref_address_id.phone
+                if self.updt_mobile:
+                    values['mobile'] = person.ref_address_id.mobile
+                if self.updt_email:
+                    values['email'] = person.ref_address_id.email
+
+                values['number'] = person.ref_address_id.number
+                values['district'] = person.ref_address_id.district
+                values['l10n_br_city_id'] = person.ref_address_id.l10n_br_city_id.id
+
+                _logger.info(u'%s %s %s', '>>>>>>>>>>', 'values:', values)
+
+                person.write(values)
 
         return True
