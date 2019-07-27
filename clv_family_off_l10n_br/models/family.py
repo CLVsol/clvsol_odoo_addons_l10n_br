9f9e25d38d612974e0c4270cd99de55f124fe6c4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-27 16:17:49 -0300

    [IMP] clv_family_off_l10n_br (IMP models/family.py and models/family_off.py)

--- clv_family_off_l10n_br/models/family.py
+++ clv_family_off_l10n_br/models/family.py
@@ -4,7 +4,7 @@
 
 import logging
 
-from odoo import api, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -12,44 +12,42 @@ _logger = logging.getLogger(__name__)
 class FamilyOff(models.Model):
     _inherit = 'clv.family_off'
 
+    use_district = fields.Boolean(
+        string='Use District',
+        help="If checked, the Family Name will contain the field District.",
+        default=True
+    )
+
     @api.multi
-    def do_family_off_get_related_person_data(self):
+    def do_family_off_get_related_family_data(self):
 
         for family_off in self:
 
-            _logger.info(u'>>>>> %s', family_off.related_person_id)
+            _logger.info(u'>>>>> %s', family_off.related_family_id)
 
             # if (family_off.reg_state in ['draft', 'revised']) and \
-            #    (family_off.related_person_id.id is not False):
-            if (family_off.related_person_id.id is not False):
+            #    (family_off.related_family_id.id is not False):
+            if (family_off.related_family_id.id is not False):
 
                 data_values = {}
-                data_values['name'] = family_off.related_person_id.name
-                data_values['code'] = family_off.related_person_id.code
-                data_values['gender'] = family_off.related_person_id.gender
-                data_values['birthday'] = family_off.related_person_id.birthday
-                data_values['responsible_id'] = family_off.related_person_id.responsible_id.id
-                data_values['caregiver_id'] = family_off.related_person_id.caregiver_id.id
+                data_values['name'] = family_off.related_family_id.name
+                data_values['code'] = family_off.related_family_id.code
 
-                if family_off.related_person_id.ref_address_id.id is not False:
+                if self.related_family_id.ref_address_id.id is not False:
 
-                    data_values['ref_address_id'] = family_off.related_person_id.ref_address_id.id
+                    data_values['ref_address_id'] = family_off.related_family_id.ref_address_id.id
 
-                    data_values['street'] = family_off.related_person_id.ref_address_id.street
-                    data_values['street_number'] = family_off.related_person_id.ref_address_id.street_number
-                    data_values['street2'] = family_off.related_person_id.ref_address_id.street2
-                    data_values['district'] = family_off.related_person_id.ref_address_id.district
-                    data_values['zip'] = family_off.related_person_id.ref_address_id.zip
-                    data_values['city'] = family_off.related_person_id.ref_address_id.city
-                    data_values['city_id'] = family_off.related_person_id.ref_address_id.city_id.id
-                    data_values['state_id'] = family_off.related_person_id.ref_address_id.state_id.id
-                    data_values['country_id'] = family_off.related_person_id.ref_address_id.country_id.id
-                    # data_values['phone'] = family_off.related_person_id.ref_address_id.phone
-                    # data_values['mobile'] = family_off.related_person_id.ref_address_id.mobile
-
-                if family_off.related_person_id.family_id.id is not False:
-
-                    data_values['family_id'] = family_off.related_person_id.family_id.id
+                    data_values['street'] = family_off.related_family_id.street
+                    data_values['street_number'] = family_off.related_family_id.street_number
+                    data_values['street2'] = family_off.related_family_id.street2
+                    data_values['district'] = family_off.related_family_id.district
+                    data_values['zip'] = family_off.related_family_id.zip
+                    data_values['city'] = family_off.related_family_id.city
+                    data_values['city_id'] = family_off.related_family_id.city_id.id
+                    data_values['state_id'] = family_off.related_family_id.state_id.id
+                    data_values['country_id'] = family_off.related_family_id.country_id.id
+                    data_values['phone'] = family_off.related_family_id.phone
+                    data_values['mobile'] = family_off.related_family_id.mobile
 
                 _logger.info(u'>>>>>>>>>> %s', data_values)
 
