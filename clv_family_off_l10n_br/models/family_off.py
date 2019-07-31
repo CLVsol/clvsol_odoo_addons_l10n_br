0c4f3afd724e8f8aadc887af0c18762ae585b3c9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-07-31 11:23:25 -0300

    [IMP] clv_family_off_l10n_br (IMP models/family_off.py)

--- clv_family_off_l10n_br/models/family_off.py
+++ clv_family_off_l10n_br/models/family_off.py
@@ -19,24 +19,24 @@ class FamilyOff(models.Model):
 
             # _logger.info(u'>>>>> %s', family_off.ref_address_id)
 
-            if (family_off.reg_state in ['draft', 'revised']):
+            # if (family_off.reg_state in ['draft', 'revised']):
 
-                data_values = {}
+            data_values = {}
 
-                data_values['street'] = False
-                data_values['street_number'] = False
-                data_values['street2'] = False
-                data_values['district'] = False
-                data_values['zip'] = False
-                data_values['city'] = False
-                data_values['city_id'] = False
-                data_values['state_id'] = False
-                data_values['country_id'] = False
-                # data_values['phone'] = False
-                # data_values['mobile'] = False
+            data_values['street'] = False
+            data_values['street_number'] = False
+            data_values['street2'] = False
+            data_values['district'] = False
+            data_values['zip'] = False
+            data_values['city'] = False
+            data_values['city_id'] = False
+            data_values['state_id'] = False
+            data_values['country_id'] = False
+            # data_values['phone'] = False
+            # data_values['mobile'] = False
 
-                _logger.info(u'>>>>>>>>>> %s', data_values)
+            _logger.info(u'>>>>>>>>>> %s', data_values)
 
-                family_off.write(data_values)
+            family_off.write(data_values)
 
         return True
