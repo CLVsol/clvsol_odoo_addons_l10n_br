cfe74b55f9a071bc8e55ae1e20c96a47febba779
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-08-17 19:09:23 -0300

    [IMP] clv_address_aux_l10n_br (IMP models/address_aux_name.py)

--- clv_address_aux_l10n_br/models/address_aux_name.py
+++ clv_address_aux_l10n_br/models/address_aux_name.py
@@ -2,11 +2,7 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-
 from odoo import api, fields, models
-
-_logger = logging.getLogger(__name__)
 
 
 class AddressAux(models.Model):
@@ -32,6 +28,3 @@ class AddressAux(models.Model):
                         record.suggested_name = record.suggested_name + ' (' + record.district + ')'
             else:
                 record.suggested_name = 'Address Name...'
-            if record.automatic_set_name:
-                if record.name != record.suggested_name:
-                    record.name = record.suggested_name
