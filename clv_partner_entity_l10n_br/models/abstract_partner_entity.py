b75636ee6736c9265b5d0ef03b9eef9b0777f85b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-10-01 21:42:16 -0300

    [IMP] clv_partner_entity_l10n_br (IMP models/abstract_partner_entity.py)

--- clv_partner_entity_l10n_br/models/abstract_partner_entity.py
+++ clv_partner_entity_l10n_br/models/abstract_partner_entity.py
@@ -13,27 +13,27 @@ class AbstractPartnerEntity(models.AbstractModel):
         self.ensure_one()
         return self.env['l10n_br.zip'].zip_search(self)
 
-    @api.multi
-    def do_set_contact_info_unavailable(self):
+    # @api.multi
+    # def do_set_contact_info_as_unavailable(self):
 
-        for record in self:
+    #     for record in self:
 
-            data_values = {}
+    #         data_values = {}
 
-            data_values['contact_info_unavailable'] = True
+    #         data_values['contact_info_is_unavailable'] = True
 
-            data_values['street'] = False
-            data_values['street_number'] = False
-            data_values['street2'] = False
-            data_values['district'] = False
-            data_values['zip'] = False
-            data_values['city'] = False
-            data_values['city_id'] = False
-            data_values['state_id'] = False
-            data_values['country_id'] = False
-            # data_values['phone'] = False
-            # data_values['mobile'] = False
+    #         data_values['street'] = False
+    #         data_values['street_number'] = False
+    #         data_values['street2'] = False
+    #         data_values['district'] = False
+    #         data_values['zip'] = False
+    #         data_values['city'] = False
+    #         data_values['city_id'] = False
+    #         data_values['state_id'] = False
+    #         data_values['country_id'] = False
+    #         # data_values['phone'] = False
+    #         # data_values['mobile'] = False
 
-            record.write(data_values)
+    #         record.write(data_values)
 
-        return True
+    #     return True
