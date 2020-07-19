02e3b485ed54d2a079441e035410942cd48e662a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-07-18 21:44:11 -0300

    [IMP] clv_address_aux_l10n_br (IMP models/address_aux.py)

--- clv_address_aux_l10n_br/models/address_aux.py
+++ clv_address_aux_l10n_br/models/address_aux.py
@@ -12,38 +12,38 @@ _logger = logging.getLogger(__name__)
 class AddressAux(models.Model):
     _inherit = 'clv.address_aux'
 
-    # @api.multi
-    def do_address_aux_get_related_address_data(self):
+    # # @api.multi
+    # def do_address_aux_get_related_address_data(self):
 
-        for address_aux in self:
+    #     for address_aux in self:
 
-            _logger.info(u'>>>>> %s', address_aux.related_address_id)
+    #         _logger.info(u'>>>>> %s', address_aux.related_address_id)
 
-            # if (address_aux.reg_state in ['draft', 'revised']) and \
-            #    (address_aux.related_address_id.id is not False):
-            if (address_aux.related_address_id.id is not False):
+    #         # if (address_aux.reg_state in ['draft', 'revised']) and \
+    #         #    (address_aux.related_address_id.id is not False):
+    #         if (address_aux.related_address_id.id is not False):
 
-                data_values = {}
-                # data_values['name'] = address_aux.related_address_id.name
-                data_values['code'] = address_aux.related_address_id.code
+    #             data_values = {}
+    #             # data_values['name'] = address_aux.related_address_id.name
+    #             data_values['code'] = address_aux.related_address_id.code
 
-                data_values['street_name'] = address_aux.related_address_id.street_name
-                data_values['street_number'] = address_aux.related_address_id.street_number
-                data_values['street2'] = address_aux.related_address_id.street2
-                data_values['district'] = address_aux.related_address_id.district
-                data_values['zip'] = address_aux.related_address_id.zip
-                data_values['city'] = address_aux.related_address_id.city
-                data_values['city_id'] = address_aux.related_address_id.city_id.id
-                data_values['state_id'] = address_aux.related_address_id.state_id.id
-                data_values['country_id'] = address_aux.related_address_id.country_id.id
-                data_values['phone'] = address_aux.related_address_id.phone
-                data_values['mobile'] = address_aux.related_address_id.mobile
+    #             data_values['street_name'] = address_aux.related_address_id.street_name
+    #             data_values['street_number'] = address_aux.related_address_id.street_number
+    #             data_values['street2'] = address_aux.related_address_id.street2
+    #             data_values['district'] = address_aux.related_address_id.district
+    #             data_values['zip'] = address_aux.related_address_id.zip
+    #             data_values['city'] = address_aux.related_address_id.city
+    #             data_values['city_id'] = address_aux.related_address_id.city_id.id
+    #             data_values['state_id'] = address_aux.related_address_id.state_id.id
+    #             data_values['country_id'] = address_aux.related_address_id.country_id.id
+    #             data_values['phone'] = address_aux.related_address_id.phone
+    #             data_values['mobile'] = address_aux.related_address_id.mobile
 
-                _logger.info(u'>>>>>>>>>> %s', data_values)
+    #             _logger.info(u'>>>>>>>>>> %s', data_values)
 
-                address_aux.write(data_values)
+    #             address_aux.write(data_values)
 
-        return True
+    #     return True
 
     # @api.multi
     def do_address_aux_clear_address_data(self):
