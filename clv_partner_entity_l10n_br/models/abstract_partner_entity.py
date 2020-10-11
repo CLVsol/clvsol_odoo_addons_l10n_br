cac061fd61c8797a0eaee7bb3359000c472bb6b6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-11 16:19:57 -0300

    [IMP] clv_partner_entity_l10n_br

--- clv_partner_entity_l10n_br/models/abstract_partner_entity.py
+++ clv_partner_entity_l10n_br/models/abstract_partner_entity.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 LasLabs Inc.
-# License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
 
@@ -8,32 +8,6 @@ from odoo import models
 class AbstractPartnerEntity(models.AbstractModel):
     _inherit = 'clv.abstract.partner_entity'
 
-    # @api.multi
     def zip_search(self):
         self.ensure_one()
         return self.env['l10n_br.zip'].zip_search(self)
-
-    # @api.multi
-    # def do_set_contact_info_as_unavailable(self):
-
-    #     for record in self:
-
-    #         data_values = {}
-
-    #         data_values['contact_info_is_unavailable'] = True
-
-    #         data_values['street_name'] = False
-    #         data_values['street_number'] = False
-    #         data_values['street2'] = False
-    #         data_values['district'] = False
-    #         data_values['zip'] = False
-    #         data_values['city'] = False
-    #         data_values['city_id'] = False
-    #         data_values['state_id'] = False
-    #         data_values['country_id'] = False
-    #         # data_values['phone'] = False
-    #         # data_values['mobile'] = False
-
-    #         record.write(data_values)
-
-    #     return True
